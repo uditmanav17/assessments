81bb8c79d0b3f4aaf82074b7ba2e6ea032585624
Udit Manav <uditmanav17@gmail.com>
2024-04-30 12:13:43 +0530

    update utility scripts

--- check_docker_hashes.py
+++ check_docker_hashes.py
@@ -1,54 +1,73 @@
-# Long pooling for updating containers from remote
+# Long polling for updating images from remote
 # scheduled via crontab on deployed server
 
 import json
 from datetime import datetime as dt
 from subprocess import PIPE, Popen
 
-images = ["uditmanav/santander_frontend:latest", "uditmanav/santander_backend:latest"]
-services = ["front_end", "backend"]
+import pytz
 
 
-for img, service in zip(images, services):
-    # fetch local docker container hash
-    cmd1 = ["docker", "images", "--no-trunc", "--quiet", f"{img}"]
-    p = Popen(cmd1, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    local_hash, err = p.communicate()
-    rc = p.returncode
-    local_hash = local_hash.decode("utf-8").strip()
-    print(f"{local_hash = }")
-    if rc != 0:
-        print(f"[{dt.now()}] Error with fetching {service} service's local hash!")
-        continue
+def update_latest_images(image_list, service_list):
+    IST = pytz.timezone("Asia/Kolkata")
+    # curr_time = dt.now(IST)
+    for img, service in zip(image_list, service_list):
+        # fetch local docker image hash
+        cmd1 = ["docker", "images", "--no-trunc", "--quiet", f"{img}"]
+        p = Popen(cmd1, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        local_hash, err = p.communicate()
+        rc = p.returncode
+        local_hash = local_hash.decode("utf-8").strip()
+        print(f"{local_hash = }")
+        if rc != 0:
+            print(
+                f"[{dt.now(IST)}] Error with fetching {service} service's local hash!"
+            )
+            continue
 
-    # fetch remote docker container hash
-    cmd2 = ["docker", "manifest", "inspect", f"{img}"]
-    p = Popen(cmd2, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    remote_hash_json, err = p.communicate()
-    rc = p.returncode
-    rh_dict = json.loads(remote_hash_json.decode("utf-8"))
-    remote_hash = rh_dict.get("config").get("digest")
-    print(f"{remote_hash = }")
-    if rc != 0:
-        print(f"[{dt.now()}] Error with fetching {service} service's remote hash!")
-        continue
+        # fetch remote docker image hash
+        cmd2 = ["docker", "manifest", "inspect", f"{img}"]
+        p = Popen(cmd2, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        remote_hash_json, err = p.communicate()
+        rc = p.returncode
+        rh_dict = json.loads(remote_hash_json.decode("utf-8"))
+        remote_hash = rh_dict.get("config").get("digest")
+        print(f"{remote_hash = }")
+        if rc != 0:
+            print(
+                f"[{dt.now(IST)}] Error with fetching {service} service's remote hash!"
+            )
+            continue
 
-    # compare hash
-    if local_hash == remote_hash:
-        print(f"[{dt.now()}] Hash matching for {service} service, nothing to do!")
-    else:
-        print(f"[{dt.now()}] Hash NOT matching for {service} service!!!")
-        print("Fetching remote container")
-        commands = [
-            ["docker", "pull", f"{img}"],
-            ["docker", "compose", "stop", f"{service}"],
-            ["docker", "compose", "up", "-d", "--no-deps", f"{service}"],
-        ]
-        for c in commands:
-            p = Popen(c, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-            output, err = p.communicate()
-            print(output)
-            print(err)
-            rc = p.returncode
-            if rc != 0:
-                print(f"[{dt.now()}] Error with updating {service}!! Please check!")
+        # compare hash
+        if local_hash == remote_hash:
+            print(
+                f"[{dt.now(IST)}] Hash matching for {service} service, nothing to do!"
+            )
+        else:
+            print(f"[{dt.now(IST)}] Hash NOT matching for {service} service!!!")
+            print("Fetching remote container")
+            commands = [
+                ["docker", "pull", f"{img}"],
+                ["docker", "compose", "stop", f"{service}"],
+                ["docker", "compose", "up", "-d", "--no-deps", f"{service}"],
+            ]
+            for c in commands:
+                p = Popen(c, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+                output, err = p.communicate()
+                print(output)
+                print(err)
+                rc = p.returncode
+                if rc != 0:
+                    print(
+                        f"[{dt.now(IST)}] Error with updating {service}!! Please check!"
+                    )
+
+
+if __name__ == "__main__":
+    images = [
+        "uditmanav/santander_frontend:latest",
+        "uditmanav/santander_backend:latest",
+    ]
+    services = ["front_end", "backend"]
+    update_latest_images(images, services)
---
+++ health_check.py
@@ -0,0 +1,40 @@
+import os
+from datetime import datetime as dt
+from subprocess import PIPE, Popen
+
+import pytz
+import requests
+
+
+def ping_server(url: str, service_name: str):
+    IST = pytz.timezone("Asia/Kolkata")
+    # curr_time = dt.now(IST)
+
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise
+        print(f"[{dt.now(IST)}] {service_name} working fine!")
+    except Exception:
+        print(f"[{dt.now(IST)}] {service_name} not reachable, restarting container!!")
+        commands = [
+            ["docker", "compose", "stop", f"{service_name}"],
+            ["docker", "compose", "up", "-d", "--no-deps", f"{service_name}"],
+        ]
+        for c in commands:
+            p = Popen(c, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            output, err = p.communicate()
+            print(output.decode("utf-8"))
+            print(err.decode("utf-8"))
+            rc = p.returncode
+            if rc != 0:
+                print(
+                    f"[{dt.now(IST)}] Error with restarting {service_name}!! Please check!"
+                )
+
+
+if __name__ == "__main__":
+    os.environ["HOST_IP"] = ""
+    HOST_IP = os.environ.get("HOST_IP", "")
+    ping_server(f"http://{HOST_IP}:8080/", "front_end")
+    ping_server(f"http://{HOST_IP}:8000/", "backend")
